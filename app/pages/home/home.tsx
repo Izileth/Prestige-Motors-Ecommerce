d66b047e5128e97a3917b61ce787c1e22bda2c4a
Kawã Kleber <kawaklebersc@gmail.com>
2025-05-15 22:23:41 -0300

    Initial Started of Global Storage

--- app/pages/home/home.tsx
+++ app/pages/home/home.tsx
@@ -3,11 +3,6 @@ import { DestactsCars } from "~/data/carousel";
 import { Testimonials } from "~/components/template/testmonial/testmonial";
 import { PartnerBrands } from "~/components/template/brands/brand";
 import { SignupCTA } from "~/components/template/cta/cta";
-import { Banner } from "~/components/layout/banner/banner";
-import { Baseboard } from "~/components/layout/baseboard/baseboard";
-import Navigation from "~/components/layout/navigation/navgation";
-import Footer from "~/components/layout/footer/footer";
-import { ReduxProviders } from "~/store/providers";
 export function Welcome() {
     return(
         <main className="flex flex-col max-w-full w-full">
