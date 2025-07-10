34fc83065288a9d33e506ef59297bceb2ae34cbc
Kawã Kleber <kawaklebersc@gmail.com>
2025-07-10 16:45:12 -0300

    Update of Clean Architeture

--- app/pages/vehicles/id/id.tsx
+++ app/pages/vehicles/id/id.tsx
@@ -1,6 +1,6 @@
 import type React from "react";
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router";
 
 import { Button } from "~/components/ui/button";
 import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
